e9166da366560d8d81a864407ecfee925f78b477
sean <shonum@gmail.com>
2024-01-05 15:42:42 +0800

    remove unnecessary revalidate secret

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,11 +4,6 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: 'Must be a POST request' })
   }
 
-  // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.SANITY_REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
-
   try {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
